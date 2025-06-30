101073f5641dd8f42b484b76c562b13bc1123aea
the-final-coomer <218564524+the-final-coomer@users.noreply.github.com>
2025-06-30 22:57:48 +1000

    fallbacks for images + fixed url

--- js/vertical_scroll.js
+++ js/vertical_scroll.js
@@ -4,7 +4,7 @@ function getBaseURL() {
 	let start = "https://i";
 
 	let maxI = 4;
-	let randomI = Math.floor(Math.random() * (maxI + 1));
+	let randomI = Math.floor(Math.random() * (maxI)) + 1;
 
 	let end = ".nhentai.net/galleries/";
 
@@ -31,10 +31,26 @@ fetch("https://nhentai.net/api/gallery/" + id)
 		// add images
 		let previous = bigcontainer;
 
-		for (let i = 1; i <= numPages; i++) {
-			let image = document.createElement('img');
+		for (let i = 1; i < numPages; i++) {
 
-			image.src = url + i + ".jpg";
+			let image = document.createElement('img');
+			
+			let fallbacks = [
+				url + i + ".webp",
+				url + i + ".jpg",
+				url + i + ".png",
+				url + i + ".gif"
+			]
+
+			let fallbackIndex = 0;
+
+			image.src = url + i + ".webp";
+			image.onerror = function(){
+				if(fallbackIndex >= fallbacks.length) return;
+				let next = fallbacks[fallbackIndex];
+				this.src = next;
+				fallbackIndex++;
+			};
 
 			previous.after(image);
 
