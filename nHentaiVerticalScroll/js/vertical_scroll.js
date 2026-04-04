2a707c854872799a0b8914fe30e9bec590f74120
the-final-coomer <218564524+the-final-coomer@users.noreply.github.com>
2026-04-05 00:32:34 +1100

    Update vertical_scroll.js
    - api v2
    - images should load more reliably

--- nHentaiVerticalScroll/js/vertical_scroll.js
+++ nHentaiVerticalScroll/js/vertical_scroll.js
@@ -1,20 +1,9 @@
 const bigcontainer = $("#bigcontainer");
 
-function getBaseURL() {
-	let start = "https://i";
-
-	let maxI = 4;
-	let randomI = Math.floor(Math.random() * (maxI)) + 1;
-
-	let end = ".nhentai.net/galleries/";
-
-	return start + randomI + end;
-}
-
 // get data
-let id = location.pathname.split("/")[2];
+const id = location.pathname.split("/")[2];
 
-fetch("https://nhentai.net/api/gallery/" + id)
+fetch("https://nhentai.net/api/v2/galleries/" + id + "/pages")
 	.then(response => {
 		if (!response.ok) {
 			throw new Error("Could not get page's data!");
@@ -23,34 +12,15 @@ fetch("https://nhentai.net/api/gallery/" + id)
 		return response.json();
 	})
 	.then(jsonData => {
-		let mediaCode = jsonData.media_id;
-		let numPages = jsonData.num_pages;
-
-		let url = getBaseURL() + mediaCode + "/";
+		const pages = jsonData.pages;
 
 		// add images
 		let previous = bigcontainer;
 
-		for (let i = 1; i < numPages; i++) {
+		for (let i = 0; i < pages.length; i++) {
 
-			let image = document.createElement('img');
-			
-			let fallbacks = [
-				url + i + ".webp",
-				url + i + ".jpg",
-				url + i + ".png",
-				url + i + ".gif"
-			]
-
-			let fallbackIndex = 0;
-
-			image.src = url + i + ".webp";
-			image.onerror = function(){
-				if(fallbackIndex >= fallbacks.length) return;
-				let next = fallbacks[fallbackIndex];
-				this.src = next;
-				fallbackIndex++;
-			};
+			const image = document.createElement('img');
+			image.src = "https://i.nhentai.net/" + pages[i].path;
 
 			previous.after(image);
 
